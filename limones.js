1299e8ac3b8a497370f585640d0b599ddb3bf1a6
Haku2h0 <Antonioworks111@gmail.com>
2026-07-05 10:57:14 -0500

    mover a la izquierda

--- limones.js
+++ limones.js
@@ -3,6 +3,7 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO= 30
 const ALTURA_PERSONAJE= 60
 const ANCHO_PERSONAJE= 40
+let personajex=canvas.width/2
 function INICIAR(){
     dibujarSuelo();
     ddibujarPersonaje();;
@@ -14,5 +15,17 @@ function dibujarSuelo(){
 }
 function ddibujarPersonaje(){
      ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE);
+    ctx.fillRect(personajex,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE);
+}
+function moverizquierda(){
+    personajex=personajex-10
+    limpiarpantalla()
+}
+function limpiarpantalla(){
+    limpiar()
+    dibujarSuelo()
+    ddibujarPersonaje()
+}
+function limpiar(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
